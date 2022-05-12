4ad4fb36d07580d5e59470113e9bb458163e1ae1
Alyxia Sother <alyxia@riseup.net>
2022-05-12 11:43:59 +0200

    [views/home] Rewrite from dropdown to inputs

--- views/home.php
+++ views/home.php
@@ -12,19 +12,30 @@ if ($connection->connect_error) {
 $activities = $connection->query("SELECT * FROM activiteit");
 $members = $connection->query("SELECT * FROM medewerker WHERE actief = 'ja'");
 
-// If we are POSTing, do the database pushing
 if (isset($_POST["name"])) {
-  $user = $connection->query("SELECT * FROM medewerker WHERE naam = '{$_POST["name"]}'")->fetch_assoc();
-  $activity = $connection->query("SELECT * FROM activiteit WHERE naam = '{$_POST['activity']}'")->fetch_assoc();
+  foreach ($_POST as $key => $value) {
+    if (!str_starts_with($key, "min-")) continue;
+    $name = str_replace("min-", "", $key);
+    $finalName = str_replace("-", " ", $name);
 
-  $connection->query("INSERT INTO urenregistratie(medewerker_id, datum, activiteit_id, minuten) VALUES ({$user["medewerker_id"]}, DATE '{$_POST['date']}', {$activity['activiteit_id']}, {$_POST['time']})");
-  if ($connection->error) {
-    throw new RuntimeException($connection->error);
+    $minuten = $_POST[$key];
+    if (!$minuten) continue;
+
+    $activiteit = $connection->query("SELECT * FROM activiteit WHERE naam = '{$finalName}'")->fetch_assoc();
+
+    $medewerker = $connection->query("SELECT * FROM medewerker WHERE naam = '{$_POST["name"]}'")->fetch_assoc();
+
+    $sql = "INSERT INTO urenregistratie (medewerker_id,datum,activiteit_id,minuten)
+     VALUES ('{$medewerker["medewerker_id"]}', DATE '{$_POST["date"]}', '{$activiteit["activiteit_id"]}', '{$minuten}')";
+
+    $connection->query($sql);
   }
 }
+
 ?>
 
 <form action="/" method="post">
+  <label>Naam</label>
   <select name="name" id="name">
     <?php
     foreach ($members as $member) {
@@ -32,16 +43,17 @@ if (isset($_POST["name"])) {
     }
     ?>
   </select>
-  <select name="activity" id="activity">
-    <?php
-    foreach ($activities as $activity) {
-      echo "<option required value='{$activity["naam"]}'>{$activity["naam"]}</option>";
-    }
-    ?>
-  </select>
+  <br />
+  <?php
+  foreach ($activities as $activity) {
+    $name = str_replace(" ", "-", $activity["naam"]);
+    echo "<label>{$activity["naam"]} </label>";
+    echo "<input placeholder='aantal minuten...' type='number' name='min-{$name}' />";
+    echo "<br>";
+  }
+  ?>
+  <br />
   <input required type="date" name="date" id="date" min=<?php echo date('Y-m-d') ?> max=<?php echo date('Y') . "-12-31" ?> />
-
-  <input required type="number" name="time" id="time" />
 
   <input type="submit" />
 </form>
