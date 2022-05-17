0973948b82450e6083e85267d3ef04358e83bc13
Alyxia Sother <alyxia@riseup.net>
2022-05-17 22:46:13 +0200

    [components/header] Implement the header

--- components/header.php
+++ components/header.php
@@ -1,3 +1,17 @@
-<?php
-
-// stub
+<div class="navbar grid grid-cols-1 md:grid-cols-3 justify-items-center bg-navbg text-navfg px-24 py-4 shadow-md mb-4">
+  <div class="flex items-center">
+    <!-- TODO: Draw a logo and use it here -->
+    <img class="inline h-6 mr-2 rounded" src="https://avatars.githubusercontent.com/u/91933425?s=200&v=4" />
+    <p class="inline" href="/"><a href="/">Tijdsregistratie</a></p>
+  </div>
+  <ul class="navbar-back <md:hidden group flex items-center pr-2">
+    <li><a href="/">Home</a></li>
+    <li><a href="/overview">Overzicht</a></li>
+    <li><a href="/overview/total">Totaal</a></li>
+  </ul>
+  <a class="flex items-center" href="https://gitlab.com/GildeCoding/yh/21-22/p1.4/135991urs" target="_blank">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" version="1.1" class="navbar-github h-6 fill-navfg <md:hidden">
+      <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+    </svg>
+  </a>
+</div>
