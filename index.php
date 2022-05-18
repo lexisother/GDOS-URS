e14ca62a902b6e59fbe1483696774d8a523fffb2
Alyxia Sother <alyxia@riseup.net>
2022-05-18 11:03:27 +0200

    [index] Split DB code into different route

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ $whoops->register();
 
 $router = new Router();
 
+session_start();
+
 // TODO: Split up POST logic to another route
 $router->all('/', function () {
   view("home");
@@ -24,6 +26,42 @@ $router->mount("/overview", function () use ($router) {
 
   $router->all("/total", function () {
     view("total");
+  });
+});
+
+$router->mount("/api", function () use ($router) {
+  $router->post("/submit", function () {
+    $connection = getConn();
+    if ($connection->connect_error) {
+      throw new RuntimeException("Connection failed: ", $connection->connect_error);
+    }
+
+    if (isset($_POST["name"])) {
+      foreach ($_POST as $key => $_) {
+        if (!str_starts_with($key, "min-")) continue;
+        $name = str_replace("min-", "", $key);
+        $finalName = str_replace("-", " ", $name);
+
+        $minuten = $_POST[$key];
+        if (!$minuten) continue;
+
+        $activiteit = $connection->query("SELECT * FROM activiteit WHERE naam = '{$finalName}'")->fetch_assoc();
+
+        $medewerker = $connection->query("SELECT * FROM medewerker WHERE naam = '{$_POST["name"]}'")->fetch_assoc();
+
+        $sql = "INSERT INTO urenregistratie (medewerker_id,datum,activiteit_id,minuten)
+     VALUES ('{$medewerker["medewerker_id"]}', DATE '{$_POST["date"]}', '{$activiteit["activiteit_id"]}', '{$minuten}')";
+
+        $connection->query($sql);
+
+        if (!$connection->error) {
+          $_SESSION["success"] = "ok";
+        } else {
+          $_SESSION["error"] = $connection->error;
+        }
+        header("Location: /");
+      }
+    }
   });
 });
 
--- views/home.php
+++ views/home.php
@@ -13,30 +13,24 @@ if ($connection->connect_error) {
 // IteratorAggregate, so it is iterable
 $activities = $connection->query("SELECT * FROM activiteit");
 $members = $connection->query("SELECT * FROM medewerker WHERE actief = 'ja'");
-
-if (isset($_POST["name"])) {
-  foreach ($_POST as $key => $value) {
-    if (!str_starts_with($key, "min-")) continue;
-    $name = str_replace("min-", "", $key);
-    $finalName = str_replace("-", " ", $name);
-
-    $minuten = $_POST[$key];
-    if (!$minuten) continue;
-
-    $activiteit = $connection->query("SELECT * FROM activiteit WHERE naam = '{$finalName}'")->fetch_assoc();
-
-    $medewerker = $connection->query("SELECT * FROM medewerker WHERE naam = '{$_POST["name"]}'")->fetch_assoc();
-
-    $sql = "INSERT INTO urenregistratie (medewerker_id,datum,activiteit_id,minuten)
-     VALUES ('{$medewerker["medewerker_id"]}', DATE '{$_POST["date"]}', '{$activiteit["activiteit_id"]}', '{$minuten}')";
-
-    $connection->query($sql);
-  }
-}
-
 ?>
 
-<form action="/" method="post">
+
+
+<?php if (isset($_SESSION["error"])) { ?>
+  <div class="notice red">
+    <p>Er ging iets mis!</p>
+  </div>
+<?php } ?>
+
+
+<?php if (isset($_SESSION["success"])) { ?>
+  <div class="notice green">
+    <p>Alles klaar!</p>
+  </div>
+<?php } ?>
+
+<form action="/api/submit" method="post">
   <label>Naam</label>
   <select name="name" id="name">
     <?php
@@ -59,3 +53,5 @@ if (isset($_POST["name"])) {
 
   <input type="submit" />
 </form>
+
+<?php $_SESSION = []; ?>
