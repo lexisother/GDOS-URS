2edfc6e1f259e16da348193c46b0d87aa2d6166b
Alyxia Sother <alyxia@riseup.net>
2022-07-05 10:22:08 +0000

    [routes] Add back the API route

--- index.php
+++ index.php
@@ -11,8 +11,12 @@ use Illuminate\Routing\UrlGenerator;
 use Whoops\Run;
 use Whoops\Handler\PrettyPageHandler;
 
+// Initialize the session
+session_start();
+
+// Setup our error handler screen
 $whoops = new Run;
-$whoops->pushHandler(new PrettyPageHandler);
+$whoops->pushHandler(new PrettyPageHandler());
 $whoops->register();
 
 // Create our service container
@@ -43,69 +47,3 @@ $response = $router->dispatch($request);
 
 // Send the response back to the browser
 $response->send();
-
-// $router = new Router();
-
-// session_start();
-
-// // TODO: Split up POST logic to another route
-// $router->all('/', function () {
-//   view("home");
-// });
-
-// $router->mount("/overview", function () use ($router) {
-//   $router->all("/", function () {
-//     view("overview");
-//   });
-
-//   $router->all("/total", function () {
-//     view("total");
-//   });
-// });
-
-// $router->mount("/api", function () use ($router) {
-//   $router->post("/submit", function () {
-//     $connection = getConn();
-//     if ($connection->connect_error) {
-//       throw new RuntimeException("Connection failed: ", $connection->connect_error);
-//     }
-
-//     if (isset($_POST["name"])) {
-//       foreach ($_POST as $key => $_) {
-//         if (!str_starts_with($key, "min-")) continue;
-//         $name = str_replace("min-", "", $key);
-//         $finalName = str_replace("-", " ", $name);
-
-//         $minuten = $_POST[$key];
-//         if (!$minuten) continue;
-
-//         $sql = "
-//           INSERT INTO
-//               urenregistratie (
-//                   medewerker_id,
-//                   datum,
-//                   activiteit_id,
-//                   minuten
-//               )
-//           VALUES (
-//               (SELECT medewerker_id FROM medewerker WHERE naam = '{$_POST["name"]}'),
-//               DATE '{$_POST["date"]}',
-//               (SELECT activiteit_id FROM activiteit WHERE naam = '{$finalName}'),
-//               {$minuten}
-//           )
-//         ";
-
-//         $connection->query($sql);
-
-//         if (!$connection->error) {
-//           $_SESSION["success"] = "ok";
-//         } else {
-//           $_SESSION["error"] = $connection->error;
-//         }
-//         header("Location: /");
-//       }
-//     }
-//   });
-// });
-
-// $router->run();
--- routes.php
+++ routes.php
@@ -17,3 +17,48 @@ $router->group(['prefix' => 'overview'], function (Router $router) {
         view('total');
     });
 });
+
+$router->group(['prefix' => 'api'], function (Router $router) {
+    $router->post("/submit", function () {
+        $connection = getConn();
+        if ($connection->connect_error) {
+            throw new RuntimeException("Connection failed: ", $connection->connect_error);
+        }
+
+        if (isset($_POST["name"])) {
+            foreach ($_POST as $key => $_) {
+                if (!str_starts_with($key, "min-")) continue;
+                $name = str_replace("min-", "", $key);
+                $finalName = str_replace("-", " ", $name);
+
+                $minuten = $_POST[$key];
+                if (!$minuten) continue;
+
+                $sql = "
+                INSERT INTO
+                    urenregistratie (
+                        medewerker_id,
+                        datum,
+                        activiteit_id,
+                        minuten
+                    )
+                VALUES (
+                    (SELECT medewerker_id FROM medewerker WHERE naam = '{$_POST["name"]}'),
+                    DATE '{$_POST["date"]}',
+                    (SELECT activiteit_id FROM activiteit WHERE naam = '{$finalName}'),
+                    {$minuten}
+                )
+                ";
+
+                $connection->query($sql);
+
+                if (!$connection->error) {
+                    $_SESSION["success"] = "ok";
+                } else {
+                    $_SESSION["error"] = $connection->error;
+                }
+                header("Location: /");
+            }
+        }
+    });
+});
